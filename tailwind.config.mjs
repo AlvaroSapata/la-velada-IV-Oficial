3223187ec6add3d4bc84e55a5e2b994e568f38e7
Alvaro Martinez <alvaromfdv@gmail.com>
2024-03-11 14:02:35 +0100

    Added font + Hero logo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+				kaushan: ["Kaushan Script", "cursive"],
+			},
       colors: {
         primary: "var(--color-primary)",
         secondary: "#var(--color-secondary)",
